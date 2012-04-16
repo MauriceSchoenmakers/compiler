9a90c4eb49d2eddfef5f9082f91431bf975ca5ff
maurice schoenmakers <maurice.schoenmakers.ext@pentasys.de>
2012-04-16 13:06:45 +0200

    added memorization of duration in expr. tree, and benchmark to show effect

--- mus/compiler.js
+++ mus/compiler.js
@@ -3,38 +3,41 @@ var x=function(t,s1,s2){for(var i=1,l=arguments.length,s;i<l;++i){s=arguments[i]
 
 var compiler=x(M={},{
 
-
     compile: function F(expr,start){if(!start)start=0;
         switch(expr.tag){
             case 'note'  : expr=x({},expr,{pitch:M.convertPitch(expr.pitch)});
             case 'rest'  : return[x(expr,{start:start})];
-            case 'seq'   : return F(expr.left,start).concat(F(expr.right,M.endTime(start,expr.left)));
+            case 'seq'   : return F(expr.left,start).concat(F(expr.right,start+M.duration(expr.left)));
             case 'par'   : return F(expr.left,start).concat(F(expr.right,start));
-            case 'repeat': return 0==expr.count?[]:F(expr.section,start).concat(F((expr.count--,expr),M.endTime(start,expr.section)));
+            case 'repeat': return 0==expr.count?[]:F(expr.section,start).concat(F((expr.count--,expr),start+M.duration(expr.section)));
         }
         return M.error('unknown tag',expr);
     },
 
-    endTime:function F(time,expr) {  // calulate end time on a mus expr based on a given start time
+    duration:x(function F(expr){ if(!expr.dur) expr.dur=F.calculate(expr,F); return expr.dur;}, // memorize result in expr
+    {calculate:function FF(expr,F){FF=F||FF; // you can also call M.duration.calculate(expr)
         switch(expr.tag){
             case 'note'  :
-            case 'rest'  :return time+expr.dur;
-            case 'seq'   :return time+F(0,expr.left)+F(0,expr.right);
-            case 'par'   :return time+Math.max(F(0,expr.left),F(0,expr.right));
-            case 'repeat':return time+expr.count*F(0,expr.section); // not really needed as done by recursion in compile
+            case 'rest'  :return expr.dur;
+            case 'seq'   :return FF(expr.left)+FF(expr.right);
+            case 'par'   :return Math.max(FF(expr.left),FF(expr.right));
+            case 'repeat':return expr.count*FF(expr.section); // not really needed as done by recursion in compile
         }
         return M.error('unknown tag',expr);
-    },
+    }}),
 
     convertPitch:x(function F(pitch){
         return 12 + 12 * parseInt(pitch[1],10) + F.letter2pitch[pitch[0]];
     },{letter2pitch:{c:0,d:2,e:4,f:5,g:7,a:9,b:11}}), // now idea where this data comes from?, see http://www.phys.unsw.edu.au/jw/notes.html
 
-    error:function(msg,expr){throw x(Error(msg+(expr&&expr.tag?': '+expr.tag:'')),{expression:expr});},
+    error:function(msg,expr){throw x(new Error(msg+(expr&&expr.tag?': '+expr.tag:'')),{expression:expr});},
 
     // loop over test set and log results
-    test:x(function F(){ for(name in F.set) try{F.log(name,F.set[name]);}catch(e){console.log("Exception:",e);} },{
-        log:function(name,t){console.log('\n\n\n=== TEST:'+name+' ===\n',t,'\n\n--- compile result ---\n');console.log(M.compile(t));},
+    test:x(function F(){ for(name in F.set) F.exec(name,F.set[name]);},{
+        exec:function F(name,t){
+            if(F.silent) try{M.compile(t);}catch(e){}
+            else try{ console.log('\n\n\n=== TEST:'+name+' ===\n',t,'\n\n--- compile result ---\n');console.log(M.compile(t));}catch(e){console.log("Exception:",e);}
+        },
 
         set:{
 
@@ -83,3 +86,16 @@ var compiler=x(M={},{
 });
 
 compiler.test();
+
+console.log('\n\n### benchmark duration(expr) ################################');
+
+var Benchmark=require('benchmark');
+
+var duration_memorize=new Benchmark(function(){compiler.duration(compiler.test.set.seq);});
+duration_memorize.run();
+
+var duration_calculate=new Benchmark(function(){compiler.duration.calculate(compiler.test.set.seq);});
+duration_calculate.run();
+
+var compare=duration_calculate.compare(duration_memorize);
+console.log('duration_calculate', (0>compare?'is slower then':(0<compare?'is faster then':'is a fast as')),'duration_memorize');
